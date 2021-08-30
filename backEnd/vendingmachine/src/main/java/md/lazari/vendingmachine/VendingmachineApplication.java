52ebc910a29d0b746261dcef8cd213a539deaa19
LazariaInk <67344017+LazariaInk@users.noreply.github.com>
2021-08-30 20:55:40 +0300

    create empty spring boot app for back-end

---
+++ backEnd/vendingmachine/src/main/java/md/lazari/vendingmachine/VendingmachineApplication.java
@@ -0,0 +1,13 @@
+package md.lazari.vendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingmachineApplication.class, args);
+	}
+
+}
---
+++ backEnd/vendingmachine/src/test/java/md/lazari/vendingmachine/VendingmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package md.lazari.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
